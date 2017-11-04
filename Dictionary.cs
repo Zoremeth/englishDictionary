000973309b954fc1dc75a1250bb68b1c1497585a
Zoremeth <zoremeth@gmail.com>
2017-11-04 14:48:31 +0100

    Inital commit

---
+++ Dictionary.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Dictionary
+    {
+        static void main()
+        {
+            
+        }
+        public void defineDictionary()
+        {
+            Dictionary <string, string> englishDictionary = new Dictionary <string, string> ();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿    using System;
+    using System.Collections.Generic;
+
+    namespace Dictionary
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Entries entry = new Entries(); 
+                Console.WriteLine("Welcome to my dictionary!");
+                entry.AddEntry();
+                //dictionary.DeleteEntry();
+                //Console.ReadKey();
+                entry.ChangeEntry();
+                entry.ListAll();
+            }   
+        }
+    }
